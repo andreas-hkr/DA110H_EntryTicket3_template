b0bb69aa9c1edf59147e9a59dcbec1bd729326a8
Andreas Nilsson <andreas.nilsson@hkr.se>
2024-07-10 18:37:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+def display_menu():
+    pass            # Replace pass with your code
+
+
+def display_suspect(suspect, graph):
+    pass            # Replace pass with your code
+
+
+def display_all_suspects(graph):
+    pass            # Replace pass with your code
+
+
+def find_potential_accomplices(suspect, graph):
+    pass            # Replace pass with your code
+
+
+def display_potential_accomplices(suspect, graph):
+    pass            # Replace pass with your code
+
+
+def load_from_file(filename):
+    pass            # Replace pass with your code
+
+
+def save_to_file(graph, filename):
+    pass            # Replace pass with your code
+
+
+def main():
+    pass            # Replace pass with your code
+
+
+if __name__ == '__main__':
+    main()
